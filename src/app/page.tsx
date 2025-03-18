6bb3403a72f8b721477f55087b75c302fc471f0a
YongHoon <akatcn@gmail.com>
2025-03-18 22:41:46 +0900

    안녕4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,6 +76,7 @@ export default function Home() {
           <p>안녕</p>
           <p>안녕2</p>
           <p>안녕3</p>
+          <p>안녕4</p>
           <Image
             aria-hidden
             src="/window.svg"
